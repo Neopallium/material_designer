90e7669c5692754895d595a71d869c4cea9a5480
Robert G. Jakabosky <rjakabosky+neopallium@neoawareness.com>
2022-01-10 01:49:06 +0800

    Add Grid mesh shape.

--- src/lib.rs
+++ src/lib.rs
@@ -2,8 +2,8 @@ use bevy::{
   prelude::*,
   reflect::TypeUuid,
   render::{
-    mesh::shape,
-    pipeline::{PipelineDescriptor, RenderPipeline},
+    mesh::{shape, Indices},
+    pipeline::{PrimitiveTopology, PipelineDescriptor, RenderPipeline},
     render_graph::{base, AssetRenderResourcesNode, RenderGraph},
     renderer::{
       RenderResource, RenderResourceType,
@@ -67,6 +67,10 @@ pub enum ObjectShape {
     uv_profile: CapsuleUvProfile,
   },
   Cube(f32),
+  Grid {
+    size: u16,
+    scale: f32,
+  },
   Icosphere {
     radius: f32,
     subdivisions: usize,
@@ -85,6 +89,55 @@ pub enum ObjectShape {
 }
 
 impl ObjectShape {
+  fn generate_grid_mesh(size: u32, scale: f32) -> Mesh {
+    let cap = (size as usize).pow(2);
+
+    // Pre-calculate position & uv units.
+    let uv_step = 1.0 / (size - 1) as f32;
+    let pos_uv = (0..size).map(|n| {
+      let pos = n as f32;
+      let uv = uv_step * n as f32;
+      (pos, uv)
+    }).collect::<Vec<_>>();
+
+    let mut positions = Vec::with_capacity(cap);
+    let mut uvs = Vec::with_capacity(cap);
+
+    for (pos_x, uv_x) in pos_uv.iter() {
+      for (pos_z, uv_z) in pos_uv.iter() {
+        let pos = [*pos_x * scale, 0.0, *pos_z * scale];
+        let uv = [*uv_x, *uv_z];
+        positions.push(pos);
+        uvs.push(uv);
+      }
+    }
+
+    let idx_cap = (cap - 2) * 2;
+    let mut indices = Vec::with_capacity(idx_cap);
+    for row_idx in 0..size-1 {
+      let top_offset = row_idx * size;
+      let btm_offset = top_offset + size;
+      if row_idx > 0 {
+        // Degenerate triangles.
+        indices.push(top_offset);
+        indices.push(btm_offset - 1);
+      }
+      for idx in (0..size).rev() {
+        // Top vertices.
+        indices.push(top_offset + idx);
+        // Bottom vertices.
+        indices.push(btm_offset + idx);
+      }
+    }
+
+    let mut mesh = Mesh::new(PrimitiveTopology::TriangleStrip);
+    mesh.set_indices(Some(Indices::U32(indices)));
+    mesh.set_attribute(Mesh::ATTRIBUTE_POSITION, positions);
+    mesh.set_attribute(Mesh::ATTRIBUTE_UV_0, uvs);
+
+    mesh
+  }
+
   fn mesh(&self) -> Mesh {
     match *self {
       ObjectShape::Box(x, y, z) =>
@@ -94,6 +147,8 @@ impl ObjectShape {
           radius, rings, depth, latitudes, longitudes,
           uv_profile: uv_profile.into(),
         }),
+      ObjectShape::Grid { size, scale } =>
+        Self::generate_grid_mesh(size as u32, scale),
       ObjectShape::Cube(size) =>
         Mesh::from(shape::Cube::new(size)),
       ObjectShape::Icosphere { radius, subdivisions } =>
